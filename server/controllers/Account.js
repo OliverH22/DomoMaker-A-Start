b2b6045065a3f948300302b3e72c9a9ce0c5bcb4
OliverH22 <42745916+OliverH22@users.noreply.github.com>
2020-10-29 15:20:51 -0400

    Add files via upload

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -1,26 +1,54 @@
 const models = require('../models');
 
-const Account = models.Account;
+const { Account } = models;
 
 const loginPage = (req, res) => {
- res.render('login');
+  res.render('login', { csrfToken: req.csrfToken() });
 };
 
-const signupPage = (req, res) => {
-const req = request;
- const res = response;
- 
- req.body.username = `${req.body.username}`;
- req.body.pass = `${req.body.pass}`;
- req.body.pass2 = `${req.body.pass2}`:
- 
- if( !req.body.username || !req.body.pass || !req.bady.pass2) {
-  return res.status(400).json({ error: 'RAWR! All fields are required' });
- }
- if(req.body.pass !== req.body.pass2){
-  return res.status(400).json({error: 'RAWR! Passwords do not match' });
- }
-   return Account.AccountModel.generateHash(req.body.pass, (salt, hash) => {
+
+const logout = (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+};
+
+const login = (request, response) => {
+  const req = request;
+  const res = response;
+
+  const username = `${req.body.username}`;
+  const password = `${req.body.pass}`;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: 'RAWR! All fields are required' });
+  }
+
+  return Account.AccountModel.authenticate(username, password, (err, account) => {
+    if (err || !account) {
+      return res.status(401).json({ error: 'Wrong username or password' });
+    }
+
+    req.session.account = Account.AccountModel.toAPI(account);
+    return res.json({ redirect: '/maker' });
+  });
+};
+
+const signup = (request, response) => {
+  const req = request;
+  const res = response;
+
+  req.body.username = `${req.body.username}`;
+  req.body.pass = `${req.body.pass}`;
+  req.body.pass2 = `${req.body.pass2}`;
+
+  if (!req.body.username || !req.body.pass || !req.body.pass2) {
+    return res.status(400).json({ error: 'RAWR! All fields are required' });
+  }
+  if (req.body.pass !== req.body.pass2) {
+    return res.status(400).json({ error: 'RAWR! Passwords do not match' });
+  }
+
+  return Account.AccountModel.generateHash(req.body.pass, (salt, hash) => {
     const accountData = {
       username: req.body.username,
       salt,
@@ -45,37 +73,10 @@ const req = request;
       return res.status(400).json({ error: 'An error occurred' });
     });
   });
- };
- 
- const logout = (req, res) => {
- res.redirect('/');
- };
- 
- const login = (request, response) => {
-  const req = request;
-  const res = response;
-
-  const username = `${req.body.username}`;
-  const password = `${req.body.pass}`;
-
-  if (!username || !password) {
-    return res.status(400).json({ error: 'RAWR! All fields are required' });
-  }
-
-  return Account.AccountModel.authenticate(username, password, (err, account) => {
-    if (err || !account) {
-      return res.status(401).json({ error: 'Wrong username or password' });
-    }
-    
-    return res.json({ redirect: '/maker' });
-  });
 };
- 
- const signup = (request, response) => { 
- };
- 
- module.exports.loginPage = loginPage;
- module.exports.login = login;
- module.exports.logout = logout;
- module.exports.signupPage = signupPage;
- module.exports.signup = signup;
+
+
+module.exports.loginPage = loginPage;
+module.exports.login = login;
+module.exports.logout = logout;
+module.exports.signup = signup;
