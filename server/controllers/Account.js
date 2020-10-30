e020b2295fa0463bdb486c80a87af19f64399679
OliverH22 <42745916+OliverH22@users.noreply.github.com>
2020-10-29 20:56:38 -0400

    Add files via upload

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -1,36 +1,17 @@
 const models = require('../models');
 
-const { Account } = models;
+const { Account } = models.Account;
 
 const loginPage = (req, res) => {
-  res.render('login', { csrfToken: req.csrfToken() });
+  res.render('login');
 };
 
+const signupPage = (req, res) => {
+  res.render('signup');
+}
 
-const logout = (req, res) => {
-  req.session.destroy();
+const logout = (req, res) => {  
   res.redirect('/');
-};
-
-const login = (request, response) => {
-  const req = request;
-  const res = response;
-
-  const username = `${req.body.username}`;
-  const password = `${req.body.pass}`;
-
-  if (!username || !password) {
-    return res.status(400).json({ error: 'RAWR! All fields are required' });
-  }
-
-  return Account.AccountModel.authenticate(username, password, (err, account) => {
-    if (err || !account) {
-      return res.status(401).json({ error: 'Wrong username or password' });
-    }
-
-    req.session.account = Account.AccountModel.toAPI(account);
-    return res.json({ redirect: '/maker' });
-  });
 };
 
 const signup = (request, response) => {
@@ -41,11 +22,12 @@ const signup = (request, response) => {
   req.body.pass = `${req.body.pass}`;
   req.body.pass2 = `${req.body.pass2}`;
 
-  if (!req.body.username || !req.body.pass || !req.body.pass2) {
-    return res.status(400).json({ error: 'RAWR! All fields are required' });
+  if (!req.body.username || !req.body.pass || !req.body.pass2){
+    return res.status(400).json({ error: 'RAWR! All fields are required'});
   }
-  if (req.body.pass !== req.body.pass2) {
-    return res.status(400).json({ error: 'RAWR! Passwords do not match' });
+
+  if(req.body.pass !== req.body.pass2){
+    return res.status(400).json({ error: 'RAWR! Passwords do not match'})
   }
 
   return Account.AccountModel.generateHash(req.body.pass, (salt, hash) => {
@@ -56,27 +38,46 @@ const signup = (request, response) => {
     };
 
     const newAccount = new Account.AccountModel(accountData);
+
     const savePromise = newAccount.save();
 
-    savePromise.then(() => {
-      req.session.account = Account.AccountModel.toAPI(newAccount);
-      return res.json({ redirect: '/maker' });
-    });
+    savePromise.then(() => res.json({ redirect: '/maker'}));
 
     savePromise.catch((err) => {
       console.log(err);
 
       if (err.code === 11000) {
-        return res.status(400).json({ error: 'Username already in use.' });
+        return res.status(400).json({ error: 'Username already in use.'});
       }
-
-      return res.status(400).json({ error: 'An error occurred' });
+      return res.status(400).json({ error: 'An error occured'});
     });
   });
 };
 
+const login = (request, response) => {
+ const req = request;
+ const res = response;
+
+ const username = `${req.body.username}`;
+ const password = `${req.body.pass}`;
+
+ if (!username || !password) {
+   return res.status(400).json({ error: 'RAWR! All fields are required'});
+   }
+
+   return Account.AccountModel.authenticate(username, password, (err, account) => {
+     if (err || !account) {
+       return res.status(401).json({ error: 'Wrong username or password'});    
+     }
+     return res.json({ redirect: '/maker'});
+   });
+};
+
+
+
 
 module.exports.loginPage = loginPage;
 module.exports.login = login;
+module.exports.signupPage = signupPage;
 module.exports.logout = logout;
 module.exports.signup = signup;
